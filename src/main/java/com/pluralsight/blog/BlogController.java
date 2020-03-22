87dfcf8d1eb7df910e132e599e854fac1230a009
steven-p-young <steven.philip.young@gmail.com>
2020-03-22 22:09:45 +0000

    Update 1

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
-    public String listPosts(ModelMap mMap){
-        @RequestMapping("/")
-        mMap.put("title","Blog Post 1");
+    @RequestMapping("/")
+    public String listPosts(ModelMap mMap) {
+        mMap.put("title", "Blog Post 1");
         return "home";
     }
 }
