32f1d734dca9f9247969bc5b176465d76fbf0955
steven-p-young <steven.philip.young@gmail.com>
2020-03-22 22:07:08 +0000

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,14 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
-
+    public String listPosts(ModelMap mMap){
+        @RequestMapping("/")
+        mMap.put("title","Blog Post 1");
+        return "home";
+    }
 }
